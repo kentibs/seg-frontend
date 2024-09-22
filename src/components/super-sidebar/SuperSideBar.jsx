39efec405c26a78372c1b907413580beba40ea25
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-09-22 05:29:22 +0300

    modify admin sidebar title to logo and media query

--- src/components/super-sidebar/SuperSideBar.jsx
+++ src/components/super-sidebar/SuperSideBar.jsx
@@ -43,9 +43,12 @@ export const SuperSideBar = () => {
       }`}
     >
       <div className={styles["seg-logo"]}>
-        <span className={styles["logo-main"]}>AAPG</span>
+        <span className={styles["logo-main"]}>
+          <img src="/aapg-logo.jpg" alt="" className={styles["aapg"]} />
+        </span>
         <span className={styles["super-admin-container"]}>
-          <span className={styles["super-admin"]}>Superadmin</span>
+          {/* <span className={styles["super-admin"]}>Superadmin</span> */}
+          <span className={styles["logo-main"]}>AAPG</span>
         </span>
       </div>
       {/* <MainNav /> */}
--- src/pages/super-users/SuperUsers.jsx
+++ src/pages/super-users/SuperUsers.jsx
@@ -145,7 +145,7 @@ export const SuperUsers = () => {
             >
               <path d="M19 11 A8 8 0 0 1 11 19 A8 8 0 0 1 3 11 A8 8 0 0 1 19 11 z" />
               <path d="M21 21l-4.35-4.35" />
-            </svg>
+            </svg> 
           </form>
         </div>
         <TableContainer
